1e5acfb424158430126d49613c02ab7185b6f804
Maciej Pędzich <dnalop.me@gmail.com>
2022-04-30 12:20:38 +0200

    Update email address

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,8 +2,10 @@
   <div class="h-full flex flex-col justify-center items-center space-y-5">
     <div class="text-center text-5xl font-bold">Contact</div>
     <div class="text-center text-xl">
-      <strong>Email address</strong>:
-      <NuxtLink to="mailto:mac@prevueland.com"> mac@prevueland.com </NuxtLink>
+      <strong>Email address: </strong>
+      <NuxtLink to="mailto:contact@maciejpedzi.ch">
+        contact@maciejpedzi.ch
+      </NuxtLink>
     </div>
   </div>
 </template>
