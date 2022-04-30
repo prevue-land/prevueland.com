d0b5f8e6d40bc5b146760989e163d1fb319a5152
Maciej Pędzich <dnalop.me@gmail.com>
2022-04-30 12:14:31 +0200

    Configure modules for newsletter, RSS and LD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,69 @@
-import { defineNuxtConfig } from 'nuxt3';
+import { defineNuxtConfig } from 'nuxt';
+import { $fetch } from 'ohmyfetch';
 import ViteFonts from 'vite-plugin-fonts';
+
+import { Article } from './models/article';
+
+const currentYear = new Date().getFullYear();
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ['highlight.js/styles/night-owl.css'],
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-social-tags', 'nuxt-directus'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt-modules/newsletter',
+    'nuxt-jsonld',
+    'nuxt-social-tags',
+    'nuxt-directus',
+    'nuxt-feed-module'
+  ],
   socialtags: {
     enabled: true,
     opengraph: true,
     twitter: true,
     locale: 'en-GB',
     theme_color: '#42b883',
-    img: 'https://pbs.twimg.com/profile_banners/1477921329546514434/1649697065/1500x500',
+    title: 'PreVue Land',
+    description: 'Home to handcrafted Vue.js and Nuxt demos',
+    img: 'https://pbs.twimg.com/profile_banners/1477921329546514434/1650568577/1500x500',
     twitter_user: 'PreVueLand',
     twitter_card: 'summary_large_image'
+  },
+  newsletter: {
+    revue: {
+      apiKey: process.env.REVUE_API_KEY,
+      component: true
+    }
+  },
+  feed: {
+    sources: [
+      {
+        meta: {
+          id: 'rss',
+          title: "PreVue Land's RSS feed",
+          link: 'https://prevueland.com/articles',
+          description: 'RSS feed with the latest PreVue Land articles',
+          copyright: `${currentYear} PreVue Land`
+        },
+        type: 'rss2',
+        path: '/rss.xml',
+        async create(feed) {
+          const { data } = await $fetch<{ data: Article[] }>(
+            `${process.env.DIRECTUS_URL}items/articles?fields=date_published,title,slug,description,content,cover&sort=-date_published&limit=3`
+          );
+
+          for (const article of data) {
+            feed.addItem({
+              id: article.slug,
+              title: article.title,
+              link: `https://prevueland.com/articles/${article.slug}`,
+              content: article.content,
+              date: new Date(article.date_published)
+            });
+          }
+        }
+      }
+    ]
   },
   vite: {
     plugins: [
